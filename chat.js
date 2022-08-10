f1feb30334167816811d247516f39e0cf0623b45
eunkeeee <eunkeeeee@gmail.com>
2022-08-10 14:31:59 +0900

    1 없애기 성공

--- chat.js
+++ chat.js
@@ -5,6 +5,7 @@ const gitReply = document.querySelector(".git_reply");
 const gitReplyTwo = document.querySelector(".git_reply_two");
 const gitReplyThree = document.querySelector(".git_reply_three");
 const gitReplyFour = document.querySelector(".git_reply_four");
+const one = document.querySelector(".did_she_read");
 const HIDDEN_CLASSNAME = "hidden";
 
 // sarangInput.value = "사랑해";
@@ -12,12 +13,16 @@ const onSubmit = (event) => {
   event.preventDefault();
   console.log(sarangInput.value);
   if (sarangInput.value.includes("사랑")) {
-    sarangHae.classList.remove("hidden");
+    sarangHae.classList.remove(HIDDEN_CLASSNAME);
+
     sarangInput.value = "";
-    setTimeout(() => gitReply.classList.remove("hidden"), 3000);
-    setTimeout(() => gitReplyTwo.classList.remove("hidden"), 4000);
-    setTimeout(() => gitReplyThree.classList.remove("hidden"), 6000);
-    setTimeout(() => gitReplyFour.classList.remove("hidden"), 10000);
+    setTimeout(() => {
+      one.classList.add(HIDDEN_CLASSNAME);
+      gitReply.classList.remove(HIDDEN_CLASSNAME);
+    }, 3000);
+    setTimeout(() => gitReplyTwo.classList.remove(HIDDEN_CLASSNAME), 4000);
+    setTimeout(() => gitReplyThree.classList.remove(HIDDEN_CLASSNAME), 6000);
+    setTimeout(() => gitReplyFour.classList.remove(HIDDEN_CLASSNAME), 10000);
   }
 };
 sarangForm.addEventListener("submit", onSubmit);
